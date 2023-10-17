5ee285d6c8e14a3f5a3093ea1da198708397056a
TashdidDiganta <tashdiddiganta@gmail.com>
2023-10-17 11:19:09 +0600

    adde

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-const dom = document.getElementById('product-container')
+const dom = document.getElementById('product-container');
+const total = document.querySelector('.total')
 
 // create arr
 const saveCartIntoLocalStorage = getDataFromLocalStorage('item') != null? getDataFromLocalStorage('item') : [];
@@ -37,25 +38,34 @@ function showCart(cart){
 /// add to cart
 function getOrderItem(id){
     const saveData = getDataFromLocalStorage('item');
-
+    let quantity = 1 ;
+   
     saveData.map(i =>{
+
         if(id === i.id){
+            let price = parseFloat(i.newProductPrice )
+
             let newDiv = document.createElement('li');
             newDiv.innerHTML = `
             <div><img src="${i.imageUrl}"/></div>
             <div>${i.newProductName}</div>
-            <div>${i.newProductPrice}</div>
+            <div>${ price }</div>
             <div class="right-margin">
-              <button>+</button>
-              <div>0</div>
-              <button>-</button>
+              <button onclick=" console.log('adf') ">-</button>
+              <div>${quantity}</div>
+              <button onclick=" ${quanssstity} ">+</button>
             </div>
           `;
-
           dom.appendChild(newDiv)
         }
-    })
-        
+    })    
 }
+
+
+function updateQuantity(){
+
+}
+
+
 
 
